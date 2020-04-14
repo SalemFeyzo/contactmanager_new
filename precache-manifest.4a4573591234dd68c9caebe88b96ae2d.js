f8ca51b5665d2892e34a77ab425cab0ff76a93b2
Salem Feyzo <f1989salim@gmail.com>
2020-04-14 11:55:54 +0300

    Updates

---
+++ precache-manifest.4a4573591234dd68c9caebe88b96ae2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdafe33ab01c5656f972da7f14d84404",
+    "url": "/contactmanager_new/index.html"
+  },
+  {
+    "revision": "4497c966d694dc11a3a4",
+    "url": "/contactmanager_new/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d8d77e993b5f4f8e3994",
+    "url": "/contactmanager_new/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "4497c966d694dc11a3a4",
+    "url": "/contactmanager_new/static/js/2.d74ef65d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager_new/static/js/2.d74ef65d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d77e993b5f4f8e3994",
+    "url": "/contactmanager_new/static/js/main.73f084bb.chunk.js"
+  },
+  {
+    "revision": "661b1ca78f64f59e4d5f",
+    "url": "/contactmanager_new/static/js/runtime-main.9682df61.js"
+  }
+]);
